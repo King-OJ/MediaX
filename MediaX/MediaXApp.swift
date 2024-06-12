616984583e7d4427579a38481ddaf5c0795c1902
Clement Ojiguo <clementojigs@hotmail.com>
2024-06-12 13:27:15 -0700

    Initial Commit

---
+++ MediaX/MediaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaXApp.swift
+//  MediaX
+//
+//  Created by King OJ on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MediaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
